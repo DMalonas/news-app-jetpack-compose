72d72229832880426deed52f00d449df27f2d3c1
Dimitrios_Malonas <dimitriosmalonas@outlook.com>
2023-06-08 15:38:07 +0000

    106.5 bottom menu added but doesnt have any navigation

--- app/src/main/java/com/example/newsapp/ui/NewsApp.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsApp.kt
@@ -1,27 +1,62 @@
 package com.example.newsapp.ui
 
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.newsapp.BottomMenuScreen
 import com.example.newsapp.MockData
+import com.example.newsapp.components.BottomMenu
+import com.example.newsapp.ui.screen.Categories
 import com.example.newsapp.ui.screen.DetailsScreen
+import com.example.newsapp.ui.screen.Sources
 import com.example.newsapp.ui.screen.TopNews
 
 @Composable
 fun NewsApp() {
-    Navigation()
+    MainScreen(rememberNavController(), rememberScrollState())
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Navigation() {
-    val navController = rememberNavController()
-    val scrollState = rememberScrollState()
+fun MainScreen(navController: NavHostController, scrollState: ScrollState) {
+    Scaffold(
+        bottomBar = {
+            BottomMenu(navController)
+        }
+    ) { innerPadding -> // padding calculated by scaffold
+        // it doesn't have to be a column,
+        // can be another component that accepts a modifier with padding
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = innerPadding) // padding applied here
+        ) {
+            // all content should be here
+            Navigation(navController, scrollState)
+        }
+    }
+}
+@Composable
+fun Navigation(navController: NavHostController, scrollState: ScrollState) {
     NavHost(navController =  navController, startDestination = "TopNews") {
+        bottomNavigation(navController)
         composable("TopNews") {
             TopNews(navController)
 //            navController.popBackStack()
@@ -33,5 +68,17 @@ fun Navigation() {
             val newsData = MockData.getNews(id)
             DetailsScreen(newsData, scrollState, navController)
         }
+    }
+}
+
+fun NavGraphBuilder.bottomNavigation(navController: NavHostController) {
+    composable(BottomMenuScreen.TopNews.route) {
+        TopNews(navController)
+    }
+    composable(BottomMenuScreen.Categories.route) {
+        Categories()
+    }
+    composable(BottomMenuScreen.Sources.route) {
+        Sources()
     }
 }
